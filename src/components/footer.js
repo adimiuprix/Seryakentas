826365657295af345518fad32bc11cc8fc033a20
Adipiter <miuprix@gmail.com>
2022-10-12 21:00:34 +0700

    add icons
    Signed-off-by: Adipiter <miuprix@gmail.com>

--- src/components/footer.js
+++ src/components/footer.js
@@ -66,6 +66,22 @@ export const Footer = () => {
                 <path d="M0 0v24h24v-24h-24zm8 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.397-2.586 7-2.777 7 2.476v6.759z" />
               </svg>
             </a>
+            <a
+              href="https://www.facebook.com/adi.pranoto.9047506/"
+              target="_blank"
+              rel="noreferrer"
+              aria-label="Facebook"
+              className="inline w-6 h-6 text-white rounded-sm fill-current hover:text-blue-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M40,0H10C4.486,0,0,4.486,0,10v30c0,5.514,4.486,10,10,10h30c5.514,0,10-4.486,10-10V10C50,4.486,45.514,0,40,0z M39,17h-3 c-2.145,0-3,0.504-3,2v3h6l-1,6h-5v20h-7V28h-3v-6h3v-3c0-4.677,1.581-8,7-8c2.902,0,6,1,6,1V17z" />
+              </svg>
+            </a>
           </div>
         </div>
       </div>
